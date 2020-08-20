429e867092c5d6adffdac7780bb43cdcd35dff6e
Christian Aldana <cdaldana90@gmail.com>
2020-08-20 11:15:58 -0500

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is a MOONLIGHT HOTEL world.");
+        }
+    }
+}
